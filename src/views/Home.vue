f8be6e0ae97ce21ed000ed96901b7f49175e4a89
qiugu <476710564@qq.com>
2019-05-15 10:09:32 +0800

    vue-infinite-scroll bug无法修复，取消使用

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,37 +2,15 @@
 <div class="home">
   <a-card title="HTML5">
     <a-card-meta title="万维网的核心语言、标准通用标记语言下的一个应用超文本标记语言（HTML）的第五次重大修改">
-      <!-- <div
-        slot="description"
-        class="demo-infinite-container"
-        v-infinite-scroll="handleInfiniteOnLoad"
-        :infinite-scroll-disabled="busy"
-        :infinite-scroll-distance="10"
-      >
-        <a-list
-          :dataSource="data"
-        >
-          <a-list-item slot="renderItem" slot-scope="item,index">
-            <a-list-item-meta :description="item.descrip">
-              <span slot="title">{{item.tag}}</span>
-              <a-avatar slot="avatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            </a-list-item-meta>
-            <div>Content</div>
-          </a-list-item>
-          <div v-if="loading && !busy" class="demo-loading-container">
-            <a-spin />
-          </div>
-        </a-list>
-      </div> -->
       <a-list
           :dataSource="data"
+          slot="description"
         >
-          <a-list-item slot="renderItem" slot-scope="item,index">
-            <a-list-item-meta :description="item.descrip">
-              <span slot="title">{{item.tag}}</span>
+          <a-list-item slot="renderItem" slot-scope="item">
+            <a-list-item-meta :title="item.tag">
               <a-avatar slot="avatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
             </a-list-item-meta>
-            <div>Content</div>
+            <div>{{item.descrip}}</div>
           </a-list-item>
           <div v-if="loading && !busy" class="demo-loading-container">
             <a-spin />
@@ -43,21 +21,13 @@
 </div>
 </template>
 <script>
-import infiniteScroll from 'vue-infinite-scroll'
-const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo'
 export default {
-  directives: { infiniteScroll },
   data () {
     return {
       data: [],
       loading: false,
       busy: false,
     }
-  },
-  beforeMount () {
-    this.fetchData((res) => {
-      this.data = res.data.resultData.results
-    })
   },
   mounted () {
     this.fetchData((res) => {
